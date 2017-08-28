19233eb49039e6bd61749ef872ac7c58dcc344ff
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-08-28 09:15:57 -0500

    fixed #20

--- src/views/Panel.js
+++ src/views/Panel.js
@@ -2,8 +2,9 @@ import m from 'mithril';
 
 import * as app from '../app';
 
-export let getClass = function(panel) {
-    if (panel.closed) return '.closepanel';
+export let getClass = panel => {
+    if (panel.closed) 
+        return '.closepanel';
     return (panel.side === 'left' && app.lefttab === 'tab2') ? '.expandpanel' : '';
 };
 
@@ -13,13 +14,14 @@ class Panel {
     }
 
     view(vnode) {
-        let dot = [m.trust('&#9679;'), m('br')];
-        return m(`#${vnode.attrs.side}panel.sidepanel.container.clearfix${getClass(this)}`, [
-            m(`#toggle${vnode.attrs.side === 'left' ? 'L' : 'R'}panelicon.panelbar[style=height: calc(100% - 60px)]`,
+        let {side, title} = vnode.attrs;
+        const dot = [m.trust('&#9679;'), m('br')];
+        return m(`#${side}panel.sidepanel.container.clearfix${getClass(this)}`, [
+            m(`#toggle${side === 'left' ? 'L' : 'R'}panelicon.panelbar[style=height: calc(100% - 60px)]`,
               m('span', {onclick: _ => this.closed = !this.closed},
                 [].concat([dot, dot, dot, dot]))),
-            m(`#${this.side}paneltitle.panel-heading.text-center`,
-              m("h3.panel-title", vnode.attrs.title))
+            m(`#${side}paneltitle.panel-heading.text-center`,
+              m("h3.panel-title", title))
         ].concat(vnode.children));
     }
 }
