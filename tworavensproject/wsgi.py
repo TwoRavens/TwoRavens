3e61b0a589735761f9f1be17b77d7198c31f680e
Raman Prasad <raman_prasad@harvard.edu>
2017-08-29 12:07:51 -0400

    updated wsgi

--- tworavensproject/wsgi.py
+++ tworavensproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tworavensproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tworavensproject.settings.dev_container")
 
 application = get_wsgi_application()
