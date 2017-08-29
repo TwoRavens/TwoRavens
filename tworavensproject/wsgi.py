e843fe9ff49809b26143ec29782a595cc887fa45
Raman Prasad <raman_prasad@harvard.edu>
2017-08-29 15:40:08 -0400

    wsgi update

--- tworavensproject/wsgi.py
+++ tworavensproject/wsgi.py
@@ -7,6 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+# ------------------------------------------------
+# virtualenvwrapper invocation
+# src: http://modwsgi.readthedocs.io/en/develop/user-guides/virtual-environments.html#daemon-mode-multiple-applications
+# ------------------------------------------------
+python_home = '/root/virtualenvs/2ravens'
+
+activate_this = python_home + '/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+# ------------------------------------------------
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
