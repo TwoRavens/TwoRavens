ca2548e554262c86dd86dd11b5509551db672365
Raman Prasad <raman_prasad@harvard.edu>
2017-08-31 22:19:26 -0400

    almost there

--- setup/init_scripts/setup_2ravens_ubuntu.sh
+++ setup/init_scripts/setup_2ravens_ubuntu.sh
@@ -3,8 +3,9 @@
 # ------------------------------------
 echo ">> Create the 2ravens virtualenv"
 # ------------------------------------
+echo VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh >> /root/.bashrc
 source /root/.bashrc
-source /usr/local/bin/virtualenvwrapper.sh
+#source /usr/local/bin/virtualenvwrapper.sh
 cd /srv/webapps/TwoRavens
 mkvirtualenv -p python3 2ravens
 pip3 install -r requirements/prod.txt
@@ -16,6 +17,8 @@ echo ">> Set postactivate script"
 export DJANGO_SETTINGS_MODULE=tworavensproject.settings.dev_container >> /root/virtualenvs/2ravens/bin/postactivate
 source /root/virtualenvs/2ravens/bin/postactivate
 
+workon 2ravens
+pip freeze
 # ------------------------------------
 echo ">> fab init_db"
 # ------------------------------------
