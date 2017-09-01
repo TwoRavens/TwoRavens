2554a795d69c9b1eefbcbe51d0ed2314c41ac6da
Raman Prasad <raman_prasad@harvard.edu>
2017-09-01 01:25:35 -0400

    updated script

--- setup/init_scripts/setup_01_pkgs.sh
+++ setup/init_scripts/setup_01_pkgs.sh
@@ -1,56 +1,36 @@
 #!/bin/bash
 
-# ------------------------------------
-echo "--> apt-utils"
-# ------------------------------------
-apt-get -y update
-apt-get install -y --no-install-recommends apt-utils
-
 # ---------------------------------------------
 # Install packages including git, apache2, and pip3
 # ---------------------------------------------
-# ---------------------------------------------
+
+echo "# ---------------------------------------------"
 echo "--> Install basic packages"
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 apt-get -y install vim
 apt-get -y install wget
 apt-get -y install net-tools
 apt-get -y install bash
 
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 echo "--> Needed for retrieving R packages"
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 apt-get -y install libssl-dev
 apt-get -y install libcurl4-openssl-dev
 apt-get -y install libxml2-dev
 
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 echo "--> To generate rook sessions numbers"
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 apt-get -y install uuid-runtime
 
-# ---------------------------------------------
-echo "--> git"
-# --------------------------------------------
-apt-get -y install git
-
-# ---------------------------------------------
+echo "# ---------------------------------------------"
 echo "--> apache (fix later: apache2-dev - to get the apxs package for mod_wsgi)"
 # --------------------------------------------
 apt-get -y install apache2-dev apache2-doc apache2-utils
 
-# ---------------------------------------------
-echo "--> python3 (fix later: apache2-dev - to get the apxs package for mod_wsgi)"
-# --------------------------------------------
+echo "# ---------------------------------------------"
+echo "--> python3"
+echo "# ---------------------------------------------"
 apt-get -y install python3-pip
 ln -sf /usr/bin/python3 /usr/bin/python
-
-
-# ---------------------------------------------
-echo "--> Clone the TwoRavens repository--the rp-django branch"
-# ---------------------------------------------
-mkdir -p /srv/webapps
-cd /srv/webapps
-git clone https://github.com/vjdorazio/TwoRavens.git
-cd TwoRavens
-git checkout rp-django
