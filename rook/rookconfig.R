54530e264c09c60cc0d757bae567cf1c424a72d6
tercer <jameshonaker2@gmail.com>
2017-09-22 01:47:07 -0400

    short version of config

--- rook/rookconfig.R
+++ rook/rookconfig.R
@@ -4,30 +4,19 @@
 production <- FALSE
 d3m_mode <- TRUE
 
-data_path <- "/rook-files/data/"
-image_path <- "/rook-files/images/"
-preprocess_path <- "/rook-files/preprocess/"
+#use_path <- "rook-files/"
+use_path <- "data/d3m/output/"
+
 
 ## Set paths where rook apps can write output
 if(production){
-	rook_output_data <- paste("/var/www/", data_path, sep="") 
-	rook_output_images <- paste("/var/www/", image_path, sep="") 
-	rook_output_preprocess <- paste("/var/www/", preprocess_path, sep="") 
+	pre_path <- "/var/www/rook/"
 	server_name <- "http://0.0.0.0"
 } else {
-	rook_output_data <- paste(getwd(), data_path, sep="") 
-	rook_output_images <- paste(getwd(), image_path, sep="") 
-	rook_output_preprocess <- paste(getwd(), preprocess_path, sep="") 
+	pre_path <- "../"
 	server_name <- "http://127.0.0.1:8080"
 }
 
-# R won't write to a directory that doesn't exist.
-if (!dir.exists(rook_output_data)){
-	dir.create(rook_output_data, recursive = TRUE)
-}
-if (!dir.exists(rook_output_images)){
-	dir.create(rook_output_images, recursive = TRUE)
-}
-if (!dir.exists(rook_output_preprocess)){
-	dir.create(rook_output_preprocess, recursive = TRUE)
-}
+
+
+
