41631be1c585f2321d86b4c6cbf2edbfde7bd6ab
Raman Prasad <raman_prasad@harvard.edu>
2017-09-23 11:55:58 -0400

    key update in test form

--- tworaven_apps/api_docs/views.py
+++ tworaven_apps/api_docs/views.py
@@ -12,7 +12,8 @@ from tworaven_apps.ta2_interfaces.models import GRPC_JSON_KEY
 def view_test_form(request):
     """View test form"""
 
-    info_dict = dict(TA2_STATIC_TEST_MODE=settings.TA2_STATIC_TEST_MODE,
+    info_dict = dict(GRPC_JSON_KEY=GRPC_JSON_KEY,
+                     TA2_STATIC_TEST_MODE=settings.TA2_STATIC_TEST_MODE,
                      TA2_TEST_SERVER_URL=settings.TA2_TEST_SERVER_URL,
                      SETTINGS_MODULE=settings.SETTINGS_MODULE)
 
