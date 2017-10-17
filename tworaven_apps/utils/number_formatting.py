895d1fc6741a23594e1090c4799c93248259608a
Raman Prasad <raman_prasad@harvard.edu>
2017-10-17 10:14:57 -0400

    fix for locale err

--- tworaven_apps/utils/number_formatting.py
+++ tworaven_apps/utils/number_formatting.py
@@ -1,14 +1,14 @@
-import locale
-locale.setlocale(locale.LC_ALL, 'en_US')
+"""convenience methods"""
 
 def add_commas_to_number(the_num):
     """Add commas to thousandths places--or return an error message"""
     if not the_num:
         return None, "You must specify a number"
-
+    #
     try:
-        return locale.format("%d", the_num, grouping=True), None
-    except TypeError as err_obj:
+        return f"{int(the_num):,d}"
+        #return locale.format("%d", the_num, grouping=True), None
+    except ValueError as err_obj:
         err_msg = ('Please specify a number (not a string'
                    ' or something else). Error: %s"') % err_obj
         return None, err_msg
