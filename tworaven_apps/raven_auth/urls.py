71d2c3e3bfba4ff4a63d6b53df19d7a5702da83d
Raman Prasad <raman_prasad@harvard.edu>
2017-10-30 15:18:18 -0400

    password reset info...#34

--- tworaven_apps/raven_auth/urls.py
+++ tworaven_apps/raven_auth/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
 
     url(r'^test-state/$', views.test_state, name='test_state'),
 
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+
 ]
