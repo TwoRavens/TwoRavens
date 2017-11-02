092bd423f6735234dc2040cba6f9249b2fe99c5c
Raman Prasad <raman_prasad@harvard.edu>
2017-11-02 09:29:55 -0400

    retrieve session key

--- tworaven_apps/workspaces/util_save_state.py
+++ tworaven_apps/workspaces/util_save_state.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 from tworaven_apps.rook_services.models import UI_KEY_SOLA_JSON, ROOK_ZESSIONID
 from tworaven_apps.workspaces.models import KEY_SESSION_WORKSPACE,\
     KEY_ZDATA, KEY_ALL_NODES, UI_KEY_ZDATA
+from tworaven_apps.utils.view_helper import get_session_key
 
 class WorkspaceUtil(object):
     """Prelim object for saving sessions"""
@@ -14,7 +15,7 @@ class WorkspaceUtil(object):
     def __init__(self, request_obj):
         """Save state based on the Django request object"""
         self.request_obj = request_obj
-
+        self.session_key = get_session_key(request_obj)
         self.update_session()
 
     def update_session(self):
@@ -40,7 +41,7 @@ class WorkspaceUtil(object):
         if UI_KEY_ZDATA in json_data:
             self.request_obj.session[KEY_ZDATA] = json_data
 
-            print ('self.request_obj.session[KEY_ZDATA]', self.request_obj.session[KEY_ZDATA])
+            print('self.request_obj.session[KEY_ZDATA]', self.request_obj.session[KEY_ZDATA])
             return True, None
 
         return False, 'No %s info to save' % UI_KEY_ZDATA
