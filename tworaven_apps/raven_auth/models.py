78a72b2ff98ffe31cef4fd4ae3b9e1227bdc0730
Raman Prasad <raman_prasad@harvard.edu>
2017-12-07 16:34:10 -0500

    added missing comma

--- tworaven_apps/raven_auth/models.py
+++ tworaven_apps/raven_auth/models.py
@@ -8,7 +8,7 @@ from django.core.serializers.json import json, DjangoJSONEncoder
 class User(AbstractUser):
     """New user class to hold extra attributes in the future"""
     FIELDS_TO_SERIALIZE = ['id', 'username',
-                           'email'
+                           'email',
                            'first_name', 'last_name',
                            'is_active', 'is_staff', 'is_superuser'
                            'last_login', 'date_joined']
