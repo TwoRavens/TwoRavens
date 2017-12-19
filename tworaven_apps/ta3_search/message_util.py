fe5492be7aa4776545a63372d0f43085d69f3efe
Raman Prasad <raman_prasad@harvard.edu>
2017-12-19 09:07:55 -0500

    message util start

---
+++ tworaven_apps/ta3_search/message_util.py
@@ -0,0 +1,7 @@
+"""Send messages to any available listeners"""
+from tworaven_apps.ta3_search.models import MessageListener
+
+
+class MessageUtil(object):
+    """Util class to send messages to MessageListener"""
+    pass
