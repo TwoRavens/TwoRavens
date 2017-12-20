12b7987179f5760cf69b0e9f2ca514d86fd82f8b
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-12-20 09:02:15 -0600

    clean up rookexplore

--- rook/rookexplore.R
+++ rook/rookexplore.R
@@ -13,7 +13,6 @@ send <- function(res) {
 explore.app <- function(env) {
   print(env)
   production <- FALSE
-  warning <- FALSE
   result <- list()
 
   if (production) {
@@ -70,7 +69,7 @@ explore.app <- function(env) {
     write(deparse(bquote(data <- read.delim(file=.(paste("data_", sessionid,".tab", sep=""))))), logfile, append=TRUE)
   } else {
     data <- read.delim("../data/fearonLaitin.tsv")
-    write("mydata <- read.delim(\"../data/fearonLaitin.tsv\")", logfile, append=TRUE)
+    write("data <- read.delim(\"../data/fearonLaitin.tsv\")", logfile, append=TRUE)
   }
 
   history <- everything$callHistory
@@ -82,185 +81,160 @@ explore.app <- function(env) {
   }
 
   tryCatch({
-    ## 1. prepare mydata so that it is identical to the representation of the data in TwoRavens
-    mydata <- executeHistory(data=mydata, history=history)
-                                        # print(mydata)
-    write("mydata <- executeHistory(data=mydata, history=history)",mylogfile,append=TRUE)
-    imageVector<<-list()
-    statistical<<-list()
-    tabular<<-list()
-    plotdata<<-list()
-    plotcount<-0
+    ## 1. prepare data so that it is identical to the representation of the data in TwoRavens
+    data <- executeHistory(data=data, history=history)
+    write("mydata <- executeHistory(data=mydata, history=history)", logfile, append=TRUE)
+    imageVector <<- list()
+    statistical <<- list()
+    tabular <<- list()
+    plotdata <<- list()
+    plotcount <- 0
 
     ## plot data
-    plotd <- mydata[,vars]
-    if(nrow(plotd)>1000) {
-      plotd <- plotd[sample(1:nrow(plotd), 1000,replace=FALSE),]
+    plotd <- data[,vars]
+    if (nrow(plotd) > 1000) {
+      plotd <- plotd[sample(1:nrow(plotd), 1000, replace=FALSE),]
     }
-    for(j in 1:ncol(plotd)) {
+    for (j in 1:ncol(plotd)) {
       plotd.coords <- plotd[,j]
-      mylab <- colnames(plotd)[j]
-      plotdata[[mylab]] <- list(varname=mylab, data=plotd.coords)
+      lab <- colnames(plotd)[j]
+      plotdata[[lab]] <- list(varname=lab, data=plotd.coords)
     }
     rm(plotd)
     ## end plot data
 
-    for (i in 1:nrow(myedges)) {
-      usepair <- unique(myedges[i,])
-      usedata <- mydata[,c(usepair)]
+    for (i in 1:nrow(edges)) {
+      usepair <- unique(edges[i,])
+      usedata <- data[,c(usepair)]
 
-      missmap<-!is.na(usedata)
-      isobserved<-apply(missmap,1,all)
-      usedata<<-usedata[isobserved,]
+      missmap <- !is.na(usedata)
+      isobserved <- apply(missmap, 1, all)
+      usedata <<- usedata[isobserved,]
 
       colv <- colnames(usedata)[2]
       rowv <- colnames(usedata)[1]
+      colvNature <- lookup[which(lookup[,1] == colv), 2]
+      rowvNature <- lookup[which(lookup[,1] == rowv), 2]
 
-      colvNature <- lookup[which(lookup[,1]==colv),2]
-      rowvNature <- lookup[which(lookup[,1]==rowv),2]
-
-                                        # what will be returned in "statistical"
-      if(colvNature!="nominal" & rowvNature!="nominal") {
-        p <-round(cor(usedata[,1],usedata[,2], use="complete.obs", method="pearson"), 4)
-        corp <- paste("Pearson correlation: ", p, sep="")
-        s <-round(cor(usedata[,1],usedata[,2], use="complete.obs", method="spearman"), 4)
-        cors <- paste("Spearman correlation: ", s, sep="")
-        k <-round(cor(usedata[,1],usedata[,2], use="complete.obs", method="kendall"), 4)
-        cork <- paste("Kendall correlation: ", k, sep="")
+      # what will be returned in "statistical"
+      if (colvNature != "nominal" & rowvNature != "nominal") {
+        p <-round(cor(usedata[,1], usedata[,2], use="complete.obs", method="pearson"), 4)
+        corp <- paste("Pearson correlation:", p, sep="")
+        s <-round(cor(usedata[,1], usedata[,2], use="complete.obs", method="spearman"), 4)
+        cors <- paste("Spearman correlation:", s, sep="")
+        k <-round(cor(usedata[,1], usedata[,2], use="complete.obs", method="kendall"), 4)
+        cork <- paste("Kendall correlation:", k, sep="")
       } else {
         myCor <- "No correlations reported"
       }
 
       statInfo <- list(var1=rowv, var2=colv, corp=corp, cors=cors, cork=cork)
-                                        # what will be returned in "tabular"
-      useTab<-usedata
+      # what will be returned in "tabular"
+      useTab <- usedata
       crosstab <- everything$zcrosstab
       var1name <- crosstab$var1.name
       var2name <- crosstab$var2.name
-
       var1value <- crosstab$var1.value
       var2value <- crosstab$var2.value
-
       var1buttontype <- crosstab$var1.buttonType
       var2buttontype <- crosstab$var2.buttonType
 
-      print(" var1 name");
-      print(var1name)
-      print(" var2 name");
-      print(var2name)
-      print(" var1 buttontype");
-      print(var1buttontype)
-      print(" var2 buttontype");
-      print(var2buttontype)
-      print(" var1 value");
-      print(var1value)
-      print(" var2 value" );
-      print(var2value)
-
-                                        # this is a default of 10 if greater than 10 unique values. eventually we can incorporate user input to define this
-      if(length(var1buttontype)==0 || length(var2buttontype)==0|| length(var1value)==0 || length(var2value)==0 || length(var2name)==0 ||length(var1name)==0) {
+      # this is a default of 10 if greater than 10 unique values. eventually we can incorporate user input to define this
+      if (length(var1buttontype) == 0
+          || length(var2buttontype) == 0
+          || length(var1value) == 0
+          || length(var2value) == 0
+          || length(var2name) == 0
+          || length(var1name) == 0)
+      {
         print("not defined value part")
-        if(length(unique(useTab[,1]))>10 & !isTRUE(rowvNature=="nominal")) {
+        if (length(unique(useTab[,1])) > 10 & !isTRUE(rowvNature == "nominal")) {
           useTab[,1] <- cut(useTab[,1], breaks=10)
         }
-        if(length(unique(useTab[,2]))>10 & !isTRUE(colvNature=="nominal")) {
+        if (length(unique(useTab[,2])) > 10 & !isTRUE(colvNature == "nominal")) {
           useTab[,2] <- cut(useTab[,2], breaks=10)
         }
-      } else {
-        if (var1buttontype == "equidistance") {
-          print("equidistance var 1  called")
-          if(length(unique(useTab[,1]))>10 & !isTRUE(rowvNature=="nominal")) {
-            useTab[,1] <- cut(useTab[,1], breaks=var1value)
-          }
-        } else if (var1buttontype == "equimass") {
-          print("equimass var 1 called")
-                                        # first arrange
-          usethis<- sort(useTab[,1], decreasing = FALSE, na.last = NA  )
-          print("equimass usethis")
-          print(usethis)
-
-          if(length(unique(useTab[,1]))>10 & !isTRUE(rowvNature=="nominal")) {
-            useTab[,1] <- cut(usethis, breaks=var1value)
-          }
+      } else if (var1buttontype == "equidistance") {
+        print("equidistance var 1  called")
+        if (length(unique(useTab[,1]))  >10 & !isTRUE(rowvNature == "nominal")) {
+          useTab[,1] <- cut(useTab[,1], breaks=var1value)
         }
-
-        if (var2buttontype == "equidistance")
-        {
-
-          print("equidistance var 2  called")
-          if(length(unique(useTab[,2]))>10 & !isTRUE(colvNature=="nominal")) {
-            useTab[,2] <- cut(useTab[,2], breaks=var2value)
-          }
-        }
-        else if(var2buttontype == "equimass")
-        {
-          print("equimass var 2  called")
-                                        # first arrange
-          usethis<- sort(useTab[,2], decreasing = FALSE, na.last = NA  )
-          print("equimass usethis")
-          print(usethis)
-          if(length(unique(useTab[,2]))>10 & !isTRUE(colvNature=="nominal")) {
-
-
-            useTab[,2] <- cut(usethis, breaks=var2value)
-          }
+      } else if (var1buttontype == "equimass") {
+        print("equimass var 1 called")
+        # first arrange
+        usethis <- sort(useTab[,1], decreasing=FALSE, na.last=NA)
+        print("equimass usethis")
+        print(usethis)
+        if (length(unique(useTab[,1])) > 10 & !isTRUE(rowvNature == "nominal")) {
+          useTab[,1] <- cut(usethis, breaks=var1value)
         }
       }
-      myTab <- table(useTab[,1],useTab[,2])
+
+      if (var2buttontype == "equidistance") {
+        print("equidistance var 2 called")
+        if (length(unique(useTab[,2])) > 10 & !isTRUE(colvNature == "nominal")) {
+          useTab[,2] <- cut(useTab[,2], breaks=var2value)
+        }
+      } else if(var2buttontype == "equimass") {
+        print("equimass var 2 called")
+        # first arrange
+        usethis <- sort(useTab[,2], decreasing = FALSE, na.last = NA  )
+        print("equimass usethis")
+        print(usethis)
+        if (length(unique(useTab[,2])) > 10 & !isTRUE(colvNature == "nominal")) {
+          useTab[,2] <- cut(usethis, breaks=var2value)
+        }
+      }
+
+      myTab <- table(useTab[,1], seTab[,2])
       rm(useTab)
       coln <- colnames(myTab)
       rown <- row.names(myTab)
       tabData <- list()
       for (j in 1:nrow(myTab)) {
-                                        #  assign(paste("row", j, sep = ""), c(rown[j],myTab[j,]))
         assign("tabData", c(tabData, list(eval(parse(text="myTab[j,]")))))
       }
       tabInfo <- list(colnames=coln, rownames=rown, colvar=colv, rowvar=rowv, data=tabData)
-      almostCall<-"plot call"
-      for(j in 1:2) {
-        plotcount<-plotcount+1
-        if(j==1){
-          spTEST <- "plot(usedata[,1],usedata[,2])"
-          plotv<-"a"
+      almostCall <- "plot call"
+      for (j in 1:2) {
+        plotcount <- plotcount + 1
+        if (j == 1) {
+          spTEST <- "plot(usedata[,1], usedata[,2])"
+          plotv <- "a"
         } else{
-          spTEST <- "plot(usedata[,2],usedata[,1])"
-          plotv<-"b"
+          spTEST <- "plot(usedata[,2], usedata[,1])"
+          plotv <- "b"
         }
 
-        if(production){
-          plotpath <- "png(file.path(\"/var/www/html/custom/pic_dir\", paste(mysessionid,\"_\",mymodelcount,i,plotv,\".png\",sep=\"\")))"
-        }else{
-          plotpath <- "png(file.path(getwd(), paste(\"output\",mymodelcount,i,plotv,\".png\",sep=\"\")))"
+        if (production) {
+          plotpath <- "png(file.path(\"/var/www/html/custom/pic_dir\", paste(sessionid, \"_\", modelcount, i, plotv, \".png\", sep=\"\")))"
+        } else {
+          plotpath <- "png(file.path(getwd(), paste(\"output\", modelcount, i, plotv, \".png\", sep=\"\")))"
         }
         eval(parse(text=plotpath))
         eval(parse(text=spTEST))
         dev.off()
-        if(production){
-          imageVector[[plotcount]]<<-paste("https://beta.dataverse.org/custom/pic_dir/", mysessionid,"_",mymodelcount,i,plotv,".png", sep = "")
-        }else{
-          imageVector[[plotcount]]<<-paste("rook/output",mymodelcount,i,plotv,".png", sep = "")
-                                        #   statistical[[plotcount]]<<-myCor
+        if (production) {
+          imageVector[[plotcount]] <<- paste("https://beta.dataverse.org/custom/pic_dir/", sessionid, "_", modelcount, i, plotv, ".png", sep="")
+        } else {
+          imageVector[[plotcount]] <<- paste("rook/output", modelcount, i, plotv, ".png", sep="")
         }
       }
-      images<-imageVector ## zplots() returns imageVector, just for consistency
-      tabular[[i]]<<-tabInfo
-      statistical[[i]] <<-statInfo
+      images <- imageVector ## zplots() returns imageVector, just for consistency
+      tabular[[i]] <<- tabInfo
+      statistical[[i]] <<- statInfo
     }
 
     if (length(images) > 0) {
-      names(images) <- apply(myedges,1,function(x) c(paste(x[1],x[2],sep="-"),paste(x[2],x[1],sep="-")))
-      names(tabular) <- names(statistical) <- apply(myedges,1,function(x) paste(x[1],x[2],sep="-"))
-      result<-list(images=images, call=almostCall, tabular=tabular, statistical=statistical, plotdata=plotdata)
+      names(images) <- apply(edges, 1, function(x) c(paste(x[1], x[2], sep="-"), paste(x[2], x[1], sep="-")))
+      names(tabular) <- names(statistical) <- apply(edges, 1, function(x) paste(x[1], x[2], sep="-"))
+      return(send(list(images=images, call=almostCall, tabular=tabular, statistical=statistical, plotdata=plotdata)))
     } else {
-      result<-list(warning="There are no graphs to show.")
+      return(send(list(warning="There are no graphs to show.")))
     }
   },
-  error = function(err){
+  error = function(err) {
     result <<- list(warning=paste("Plot error: ", err))
   })
   return(send(result))
 }
-
-
-
-
