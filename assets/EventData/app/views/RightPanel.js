8e99863a54a8f33879e8a24b0c46d4b23326586e
Marcus D <MarcusD.9234@gmail.com>
2018-01-05 18:32:25 -0600

    included renamed files to fix module not found error

---
+++ assets/EventData/app/views/RightPanel.js
@@ -0,0 +1,90 @@
+import m from 'mithril';
+import {download} from "../app";
+
+export default class Rightpanel {
+    view(vnode) {
+        return (m(".sidepanel.container.clearfix[id='rightpanel']",
+            [
+                m(".panelbar[id='toggleRpanelicon']",
+                    m("span",
+                        [
+                            m.trust("&#9679;"),
+                            m("br"),
+                            m.trust("&#9679;"),
+                            m("br"),
+                            m.trust("&#9679;"),
+                            m("br"),
+                            m.trust("&#9679;")
+                        ]
+                    )
+                ),
+                m(".panel-heading.text-center[id='rightpaneltitle']",
+                    m("h3.panel-title",
+                        "Query Summary"
+                    )
+                ),
+                m("[id='queryVariables']", {
+                        style: {
+                            "margin-left": "5px",
+                            "width": "232px",
+                            "height": "calc(35% - 39px)",
+                            "overflow-y": "auto"
+                        }
+                    },
+                    [
+                        m("h4.panel-title",
+                            "Variables"
+                        ),
+                        m("[id='variableTree']", {
+                            style: {
+                                "margin-left": "10px",
+                                "width": "calc(100% - 15px)",
+                                "height": "calc(100% - 19px)",
+                                "overflow-y": "auto"
+                            }
+                        })
+                    ]
+                ),
+                m("[id='querySubsets']", {style: {"width": "232px", "height": "calc(65% - 39px)"}},
+                    [
+                        m("h4.panel-title", {style: {"margin-left": "5px"}},
+                            "Subsets"
+                        ),
+                        m("[id='subsetTree']", {style: {"height": "calc(100% - 19px)", "overflow-y": "auto"}})
+                    ]
+                ),
+                m("[id='rightpanelButtonBar']", {style: {"width": "232px", "position": "absolute", "bottom": "5px"}},
+                    [
+                        m("button.btn.btn-default[id='buttonAddGroup'][type='button']", {
+                                style: {
+                                    "float": "left",
+                                    "margin-left": "6px"
+                                },
+                                onclick: function (e) {
+                                    addGroup();
+                                    e.redraw = false;
+                                }
+                            },
+                            "Group"
+                        ),
+                        m("button.btn.btn-default.ladda-button[data-spinner-color='#818181'][id='buttonDownload'][type='button']", {
+                                style: {
+                                    "float": "right",
+                                    "margin-right": "6px",
+                                    "data-style": "zoom-in"
+                                },
+                                onclick: function (e) {
+                                    download();
+                                    e.redraw = false;
+                                }
+                            },
+                            m("span.ladda-label",
+                                "Download"
+                            )
+                        )
+                    ]
+                )
+            ]
+        ));
+    }
+}
