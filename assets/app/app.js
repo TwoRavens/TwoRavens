ba7bfbc6dbd42880f539afefcd5c330a22be6396
tercer <jameshonaker2@gmail.com>
2018-01-25 23:12:34 -0500

    solving error where removing features caused crash

--- assets/app/app.js
+++ assets/app/app.js
@@ -2030,7 +2030,7 @@ export async function estimate(btn) {
         if (!res) {
             estimated = true;
         } else {
-        console.log(res);
+        console.log(res.predictors);
             setxTable(res.predictors);
             let dvvalues = res.dvvalues;
             res = await makeRequest(D3M_SVC_URL + '/CreatePipelines', CreatePipelineData(res.predictors, res.depvar));
@@ -3679,24 +3679,23 @@ export function setxTable(features) {
         return table;
     }
 
-
     let mydata = [];
     for(let i = 0; i<features.length; i++) {
-        if(allNodes[findNodeIndex(features[i])].valid==0) {
+        let myi = findNodeIndex(features[i]); //i+1;                                // This was set as (i+1), but should be allnodes position, not features position
+
+        if(allNodes[myi].valid==0) {
             xval=0;
             x1val=0;
             mydata.push({"Variables":features[i],"From":xval, "To":x1val});
             continue;
         }
 
-        let myi = i+1;
         let mysvg = features[i]+"_setxLeft_"+myi;
+        //console.log(mysvg);
         let xval = byId(mysvg).querySelector('.xval').innerHTML;
         let x1val = byId(mysvg).querySelector('.x1val').innerHTML;
         xval = xval.split("x: ").pop();
         x1val = x1val.split("x1: ").pop();
-        console.log(xval);
-        console.log(mysvg);
 
         mydata.push({"Variables":features[i],"From":xval, "To":x1val});
     }
