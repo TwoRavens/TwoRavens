ff2a9f88ef44f610f503dff0e58d3754af3dd1bf
tercer <jameshonaker2@gmail.com>
2018-02-26 15:53:37 -0500

    more adjustments/fixes for confusion matrix

--- assets/app/app.js
+++ assets/app/app.js
@@ -4234,7 +4234,7 @@ export function confusionmatrix(matrixdata, classes) {
     let condiv = document.createElement('div');
     condiv.id="confusioncontainer";
     condiv.style.display="inline-block";
-    condiv.style.width=+(mainwidth*.75)+'px';   // Need to not be hard coded
+    condiv.style.width=+(mainwidth*.4)+'px';   // Need to not be hard coded
     condiv.style.marginLeft='20px';
     condiv.style.height=+(mainheight)+'px';     // Need to not be hard coded
     condiv.style.float="left";
@@ -4242,9 +4242,9 @@ export function confusionmatrix(matrixdata, classes) {
 
     let legdiv = document.createElement('div');
     legdiv.id="confusionlegend";
-    legdiv.style.width=+(mainwidth*.07)+'px';
+    legdiv.style.width=+(mainwidth*.07)+'px'; // Need to not be hard coded
     legdiv.style.marginLeft='20px';
-    legdiv.style.height=+(mainheight*.4)+'px';
+    legdiv.style.height=+(mainheight)+'px';   // Need to not be hard coded
     legdiv.style.display="inline-block";
     byId('setxLeftPlot').appendChild(legdiv);
 
@@ -4493,8 +4493,8 @@ export function confusionmatrix(matrixdata, classes) {
            labels    : classes,
            start_color : '#ffffff',
            end_color : '#e67e22',
-           width : mainwidth * .45,      // Need to not be hard coded
-           height : mainheight * .625,    // Need to not be hard coded
+           width : mainwidth * .35,      // Need to not be hard coded
+           height : mainheight * .6,    // Need to not be hard coded
            widthLegend : mainwidth*.05
            });
 
