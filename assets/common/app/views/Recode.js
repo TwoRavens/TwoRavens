d2a1632b19025cdb0cf38fe8b5983235cd36718b
Mital188 <mital.modha@hotmail.com>
2018-06-06 22:53:53 -0500

    create Variable UI added

--- assets/common/app/views/Recode.js
+++ assets/common/app/views/Recode.js
@@ -46,9 +46,7 @@ export default class Recode {
 
         this.configuration = localStorage.getItem('configuration');
         config = this.configuration;
-        console.log('config')
-        console.log(config)
-//data/ traindata.csv
+        //data/ traindata.csv
         m.request(config).then(data => {
             
             Object.assign(dataDetails,  data['variables']);
@@ -75,108 +73,142 @@ export default class Recode {
                         ]),
                     ])
                 ]),
-            m(Panel, {
-                side: 'left',
-                label: 'Data Selection',
-                hover: true,
-                width: app.modelLeftPanelWidths[app.leftTab],
-                attrsAll: {style: {'z-index': 101, 'overflow':'scroll'}}
-            },  m(PanelList,{
-                id: 'varList',
-                items: varList,
-                callback: clickVar,
-                
-            }),
-        ),
-        m(`#${'right'}panel.container.sidepanel.clearfix`, mergeAttributes({
-            style: {
-                overflow: 'scroll',
-                background: menuColor,
-                border: borderColor,
-                width: (window.innerWidth - 300)+'px' ,
-                height: `calc(100% - ${heightHeader + heightFooter}px - ${2 * panelMargin}px - ${canvasScroll['horizontal'] ? scrollbarWidth : 0}px)`,
-                position: 'fixed',
-                top: heightHeader + panelMargin  + 'px',
-            }}),
-            m('div.container-fluid ',{id: 'recodeDiv'},[
-
-                m('div.container-fluid', {id : 'div1_recode' , style : {'height': '220px','padding':'20px'}}, [
-                    m('form',{ onsubmit: calculate},[
-                        m('div',{style :{'display': 'block','overflow': 'hidden'}},[
-                            m('input[type=text]', {id: 'variable', placeholder: 'Variable', style : {'width': '100%','box-sizing':'border-box'}}),
-                            m('span',{ onmouseover:_=> showTooltip() , onmouseout:_=> hideTooltip(),style: {'position': 'absolute','display': 'inline','top': '25px','color': 'grey','padding-left': '5px', 'font-size':'17px'}},"?"),
-                            m('div',{id:'tooltip', style: 'right:0; display: none; margin-right: 35px'},[
-                                m('table',[
-                                    m('tr',[
-                                        m('th','Use..'),
-                                        m('th','to do...'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'sqrt('+currentVal+')'),
-                                        m('td','square root of a variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'abs('+currentVal+')'),
-                                        m('td','absolute value of the variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'exp('+currentVal+')'),
-                                        m('td','exponential of the variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'log('+currentVal+')'),
-                                        m('td','logarithmic value of a variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'log10('+currentVal+')'),
-                                        m('td','logarithmic base 10 value of a variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'factorial('+currentVal+')'),
-                                        m('td','factorial of a variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'ceiling('+currentVal+')'),
-                                        m('td','ceiling value of a variable'),
-                                    ]),
-                                    m('tr',[
-                                        m('td',{style:{'font-family': 'Courier New'}},'floor('+currentVal+')'),
-                                        m('td','floor value of a variable'),
-                                    ]),
-                                ])
-                            ]),                            
-                        ]),
-                        m("br"),
-                        m('button[type="submit"]',{style: {'float':'right'}}, 'Customize'),
-                    ]),]),
-                    m('div.container-fluid', {id : 'div2' , style : {'display':'block','height': '250px','padding':'20px'}}, [
-                    m('form',{ onsubmit: calculate2},[
-                        m('span',{style :{'display': 'block','overflow': 'hidden'}},[
-                            m('input[type=text]', {id: 'value1', placeholder: 'value 1', style : {'display':'inline-block' , 'margin-right':'10px', 'width':'20%'}}),
-                            m('h3',{ style : {'display':'inline-block', 'margin-right':'10px'}},'-'),
-                            m('input[type=text]', {id: 'value2', placeholder: 'value 1', style : {'display':'inline-block' , 'margin-right':'10px', 'width':'20%'}}),
-                            m('h3',{ style : {'display':'inline-block', 'margin-right':'10px'}},'='),
-                            m('input[type=text]', {id: 'newValue', placeholder: 'New Value', style : {'display':'inline-block' , 'width':'20%'}}),
-                        ]),
+                m('div.outter',{style:{'padding':'10px'}},[
+                    m(Panel, {
+                        side: 'left',
+                        label: 'Data Selection',
+                        hover: true,
+                        width: app.modelLeftPanelWidths[app.leftTab],
+                        attrsAll: {style: {'z-index': 101, 'overflow':'scroll'}}
+                    },  m(PanelList,{
+                        id: 'varList',
+                        items: varList,
+                        callback: clickVar,
                         
-                        m("br"),
-                        m('button[type="submit"]',{style: {'float': 'right'}} ,'Customize'),
+                    }),
+                ),
+                m(`#rightpanel.container.sidepanel.clearfix`, mergeAttributes({
+                    style: {
+                        overflow: 'scroll',
+                        background: menuColor,
+                        border: borderColor,
+                        width: (window.innerWidth - 300)+'px' ,
+                        height: `calc(100% - ${heightHeader + heightFooter}px - ${2 * panelMargin}px - ${canvasScroll['horizontal'] ? scrollbarWidth : 0}px)`,
+                        position: 'fixed',
+                        top: heightHeader + panelMargin  + 'px',
+                    }}),
+                    m('div.container-fluid ',{id: 'recodeDiv'},[
+        
+                        m('div.container-fluid', {id : 'div1_recode' , style : {'height': '220px','padding':'20px'}}, [
+                            m('form',{ onsubmit: calculate},[
+                                m('div',{style :{'display': 'block','overflow': 'hidden'}},[
+                                    m('input[type=text]', {id: 'variable', placeholder: 'Variable', style : {'width': '100%','box-sizing':'border-box'}}),
+                                    m('span',{ onmouseover:_=> showTooltip() , onmouseout:_=> hideTooltip(),style: {'position': 'absolute','display': 'inline','top': '25px','color': 'grey','padding-left': '5px', 'font-size':'17px'}},"?"),
+                                    m('div',{id:'tooltip', style: 'right:0; display: none; margin-right: 35px'},[
+                                        m('table',[
+                                            m('tr',[
+                                                m('th','Use..'),
+                                                m('th','to do...'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'sqrt('+currentVal+')'),
+                                                m('td','square root of a variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'abs('+currentVal+')'),
+                                                m('td','absolute value of the variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'exp('+currentVal+')'),
+                                                m('td','exponential of the variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'log('+currentVal+')'),
+                                                m('td','logarithmic value of a variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'log10('+currentVal+')'),
+                                                m('td','logarithmic base 10 value of a variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'factorial('+currentVal+')'),
+                                                m('td','factorial of a variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'ceiling('+currentVal+')'),
+                                                m('td','ceiling value of a variable'),
+                                            ]),
+                                            m('tr',[
+                                                m('td',{style:{'font-family': 'Courier New'}},'floor('+currentVal+')'),
+                                                m('td','floor value of a variable'),
+                                            ]),
+                                        ])
+                                    ]),                            
+                                ]),
+                                m("br"),
+                                m('button[type="submit"]',{style: {'float':'right'}}, 'Customize'),
+                            ]),]),
+                            m('div.container-fluid', {id : 'div2' , style : {'display':'block','height': '250px','padding':'20px'}}, [
+                            m('form',{ onsubmit: calculate2},[
+                                m('span',{style :{'display': 'block','overflow': 'hidden'}},[
+                                    m('input[type=text]', {id: 'value1', placeholder: 'value 1', style : {'display':'inline-block' , 'margin-right':'10px', 'width':'20%'}}),
+                                    m('h3',{ style : {'display':'inline-block', 'margin-right':'10px'}},'-'),
+                                    m('input[type=text]', {id: 'value2', placeholder: 'value 1', style : {'display':'inline-block' , 'margin-right':'10px', 'width':'20%'}}),
+                                    m('h3',{ style : {'display':'inline-block', 'margin-right':'10px'}},'='),
+                                    m('input[type=text]', {id: 'newValue', placeholder: 'New Value', style : {'display':'inline-block' , 'width':'20%'}}),
+                                ]),
+                                
+                                m("br"),
+                                m('button[type="submit"]',{style: {'float': 'right'}} ,'Customize'),
+                            ])
+                        ])
+                    
+                    ]),
+                    m('div.container-fluid', {id: 'formulaDiv'},[
+        
+                        m('div.container-fluid', {id : 'div1' , style : {'display':'block','height': '220px','padding':'20px'}}, [
+                            m('form',{ onsubmit: createNewCalculate},[
+                                m('span',{style :{'display': 'block','overflow': 'hidden'}},[m('input[type=text]', {id: 'variables', placeholder: 'Variable', style : {'width': '100%','box-sizing':'border-box','white-space': 'nowrap;'}}),]),
+                                m("br"),
+                                m('button[type="submit"]',{style: {'float':'right'}}, 'Customize'),
+                            ]),])
                     ])
-                ])
-            
-            ]),
-            m('div.container-fluid', {id: 'formulaDiv'},[
-
-                m('div.container-fluid', {id : 'div1' , style : {'display':'block','height': '220px','padding':'20px'}}, [
-                    m('form',{ onsubmit: createNewCalculate},[
-                        m('span',{style :{'display': 'block','overflow': 'hidden'}},[m('input[type=text]', {id: 'variables', placeholder: 'Variable', style : {'width': '100%','box-sizing':'border-box'}}),]),
-                        m("br"),
-                        m('button[type="submit"]',{style: {'float':'right'}}, 'Customize'),
-                    ]),])
-            ])
-        ), 
-    ]}
-}
+                ),
+                m('#centralPanel.container.sidepanel.clearfix', mergeAttributes({
+                    style: {
+                        overflow: 'hidden',
+                        background: menuColor,
+                        border: borderColor,
+                        width: (window.innerWidth-25)+'px' ,
+                        height: `calc(100% - ${heightHeader + heightFooter}px - ${2 * panelMargin}px - ${canvasScroll['horizontal'] ? scrollbarWidth : 0}px)`,
+                        position: 'fixed',
+                        display:'block',
+                        top: heightHeader + panelMargin  + 'px',
+                        
+                    }}),[
+                        m('div.container-fluid', {id : 'createDiv' , style : {'display':'block','height': '250px','padding':'20px'}}, [
+                            m('form',{ onsubmit: createNewCalculate},[
+                                m('span',{style :{'display': 'block','overflow': 'hidden'}},[
+                                    m('h4',{ style : {'display':'inline-block', 'margin-right':'10px'}},'Variable Name : '),
+                                    m('input[type=text]', {id: 'newVar', placeholder: 'New Variable', style : {'display':'inline-block' , 'margin-right':'10px', 'width':'20%'}}),
+                                    m('br'),
+                                    m('h4',{ style : {'display':'inline-block', 'margin-right':'10px'}},'Variable Type : '),
+                                    // m('input[type=text]', {id: 'newValue', placeholder: 'New Value', style : {'display':'inline-block' , 'width':'20%'}}),
+                                    m('div.dropdown',{ style : {'display':'inline-block'}},[
+                                        m('select.form-control',{style:{'display':'inline-block'}, id:'varType'},[
+                                            m('option','numchar')
+                                        ])
+                                    ])
+                                ]),
+                                
+                                m("br"),
+                                m('button[type="submit"]' ,'Create New Variable'),
+                            ])
+                        ])
+                    ])
+                ]),
+            ]}
+        }
 
 function onRecodeStorageEvent(recode, e){
     recode.configuration = localStorage.getItem('configuration');
@@ -272,19 +304,43 @@ function createClick(){
     elem.className = '';
     var elem = document.getElementById('reorderLink');
     elem.className = '';
+
+    var elem = document.getElementById('leftpanel');
+    elem.style.display ='none';
+    
+    var elem = document.getElementById('rightpanel');
+    elem.style.display ='none';
+    
+    var elem = document.getElementById('centralPanel');
+    elem.style.display ='block';
+    
 }
 function createCreate(){
     var elem = document.getElementById('formulaDiv');
     elem.style.display = 'none';
+    var elem = document.getElementById('centralPanel');
+    elem.style.display ='none';
 }
 
 function recodeCreate(){
     var elem = document.getElementById('recodeLink');
     elem.className = 'active';
+
+    var elem = document.getElementById('centralPanel');
+    elem.style.display ='none';
 }
 function recodeClick(){
     var elem = document.getElementById('recodeLink');
     elem.className = 'active';
+
+    var elem = document.getElementById('centralPanel');
+    elem.style.display="none"
+
+    var elem = document.getElementById('rightpanel');
+    elem.style.display="block"
+
+    var elem = document.getElementById('leftpanel');
+    elem.style.display="block"
 
     var elem = document.getElementById('recodeDiv');
     elem.style.display="block";
@@ -300,8 +356,18 @@ function recodeClick(){
     elem.className = '';
 }
 
-function formulaCreate(){}
+function formulaCreate(){
+
+    var elem = document.getElementById('centralPanel');
+    elem.style.display ='none';
+}
 function formulaClick(){
+
+    var elem = document.getElementById('rightpanel');
+    elem.style.display="block"
+
+    var elem = document.getElementById('leftpanel');
+    elem.style.display="block"
 
     var elem = document.getElementById('formulaDiv');
     elem.style.display="block";
@@ -330,11 +396,18 @@ function reorderClick(){
     elem.className = '';
     
 }
-function reorderCreate(){}
+function reorderCreate(){
+
+    var elem = document.getElementById('centralPanel');
+    elem.style.display ='none';
+}
 
 $(window).resize(function(){
     $('#rightpanel').css({
     'width':  (window.innerWidth - 300)+'px',
+});
+$('#centralPanel').css({
+    'width':  (window.innerWidth-25)+'px',
 });
 });
 
