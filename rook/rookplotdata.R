38a0b4847b2d74e9618feff4e1e2c11e21dd31d0
vjdorazio <dorazio@iq.harvard.edu>
2018-06-16 06:05:53 -0500

    plottype scatter-org and scatter

--- rook/rookplotdata.R
+++ rook/rookplotdata.R
@@ -46,7 +46,7 @@ plotdata.app <- function(env) {
 
     mydata <- read.csv(dataurl)
   
-    if(plottype=="scatter") {
+    if(plottype=="scatter-org") {
         tryCatch({
         plotdata <<- list()
 
@@ -75,6 +75,31 @@ plotdata.app <- function(env) {
   }
   
   if(plottype=="box") {
+        tryCatch({
+        plotdata <<- list()
+
+        ## plot data
+        plotd <- mydata[,vars]
+        plotd <- na.omit(plotd)
+        if (nrow(plotd) > 1000) {
+            plotd <- plotd[sample(1:nrow(plotd), 1000, replace=FALSE),]
+        }
+        
+        plotdata <- jsonlite:::toJSON(plotd)
+        rm(plotd)
+        
+        if (length(plotdata) !=0) {
+            return(send(list(plottype=plottype, vars=vars, plotdata=plotdata)))
+        } else {
+            return(send(list(warning="No plot data.")))
+        }
+        },
+        error = function(err) {
+        result <<- list(warning=paste("error: ", err))
+        })
+  }
+  
+  if(plottype=="scatter") {
         tryCatch({
         plotdata <<- list()
 
