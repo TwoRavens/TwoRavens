d905fdea1c87fe4820ee7cdbba828b29df4ab683
Raman Prasad <raman_prasad@harvard.edu>
2018-07-26 08:25:34 -0400

    fix for #341

--- rook/rookzeligrestart.R
+++ rook/rookzeligrestart.R
@@ -4,7 +4,10 @@
 ##  simple restart of the R.server
 ##
 
-R.server$stop()
+print('> restart step 1')
+#R.server$stop()
 R.server$remove(all=TRUE)
 rm(list=ls())
+
+print('> restart step 2')
 source("rooksource.R")
