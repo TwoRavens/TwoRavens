437f696a028798ac93abe82968ada937a28d997b
Raman Prasad <raman_prasad@harvard.edu>
2018-07-30 15:53:29 -0400

    added time to rook healthcheck

--- rook/rookhealthcheck.R
+++ rook/rookhealthcheck.R
@@ -1,36 +1,14 @@
 ##
 ##  rookhealthcheck.R
 ##
-##  Return a response to show the server is up
+##  Return a response to show the server is running
 ##
-
-
-
 healthcheck.app <- function(env){
 
-    #sink();
     source("rookconfig.R")
 
-    #request <- Request$new(env)
-
-    #print('(1)----------------------------------------')
-    #print(request$POST())
-
-    #print('(2)----------------------------------------')
-    #print(request$POST()$solaJSON)
-
-    #print('(3)----------------------------------------')
-    #valid <- jsonlite::validate(request$POST()$solaJSON)
-    #print(valid)
-
-    #print('(4)----------------------------------------')
-    #everything <- jsonlite::fromJSON(request$POST()$solaJSON)
-    #print(everything)
-    #print('----------------------------------------')
-
-    #request <- Request$new(env)
     response <- Response$new(headers = list( "Access-Control-Allow-Origin"="*"))
 
-    response$write("rook is up")
+    response$write(paste("rook is running<br />(", Sys.time(), ")", sep=""))
     response$finish()
 }
