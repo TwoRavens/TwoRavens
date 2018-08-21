72296d0be02c64e0dec00deb01e04cc926c31fc3
Raman Prasad <raman_prasad@harvard.edu>
2018-08-21 17:18:54 -0400

    try with github redirect_url

--- tworavensproject/settings/event_data_gce.py
+++ tworavensproject/settings/event_data_gce.py
@@ -2,14 +2,30 @@ from .dev_container2 import *
 from distutils.util import strtobool
 import os
 
+
+# -----------------------------------
+# Override some basic settings
+# -----------------------------------
 DEBUG = strtobool(os.environ.get('DEBUG', 'False'))
 
+# staticfiles served via nginx
+#
 STATIC_ROOT = join('/ravens_volume', 'staticfiles', 'static')
 if not os.path.isdir(STATIC_ROOT):
     os.makedirs(STATIC_ROOT)
 
 SESSION_COOKIE_NAME = os.environ.get('RAVENS_SESSION_COOKIE_NAME',
                                      'gce_event_data')
+
+# -----------------------------------
+# Social Auth related for github login
+# -----------------------------------
+GITHUB_REDIRECT_URI = os.environ.get(\
+                        'GITHUB_REDIRECT_URI',
+                        'http://eventdata.2ravens.org/auth-s/complete/github/')
+
+SOCIAL_AUTH_GITHUB_AUTH_EXTRA_ARGUMENTS = dict(redirect_uri=GITHUB_REDIRECT_URI)
+
 
 # -----------------------------------
 # use Google Cloud MySQL
