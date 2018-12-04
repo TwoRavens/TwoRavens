e392ef51961ca70874392e4f890fb02f3d5a2de6
Raman Prasad <raman_prasad@harvard.edu>
2018-12-04 09:57:55 -0500

    adjusting to serve static files via nginx #436

--- tworavensproject/settings/gce_settings.py
+++ tworavensproject/settings/gce_settings.py
@@ -5,6 +5,16 @@ import os
 from .dev_container2 import *
 
 # -----------------------------------
+# Use host forwarded from nginx
+# -----------------------------------
+USE_X_FORWARDED_HOST = True
+ALLOWED_HOSTS = ('*',) #('2ravens.org', )
+
+#DEBUG = strtobool(os.environ.get('DEBUG', 'False'))
+
+DEBUG = False
+
+# -----------------------------------
 # use Google Cloud MySQL
 # -----------------------------------
 DATABASES = {
@@ -20,7 +30,22 @@ DATABASES = {
 
 SWAGGER_HOST = '2ravens.org'
 
+
+# -----------------------------------
+# staticfiles served via nginx
+# -----------------------------------
+STATIC_ROOT = join('/ravens_volume', 'staticfiles', 'static')
+if not os.path.isdir(STATIC_ROOT):
+    os.makedirs(STATIC_ROOT)
+
+SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME',
+                                     '2ravens_metadata_gce')
+CSRF_COOKIE_NAME = '2ravens_metadata_gce_csrf'
+
 # -----------------------------------
 # Note: SECRET_KEY and MONGO_CONNECTION_STRING
 #  are loaded from tworavens-web-secrets.yml
 # -----------------------------------
+
+# for testing
+TA2_STATIC_TEST_MODE = True
