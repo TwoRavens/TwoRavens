8673f99c59e93f6291a66f2719ad6cdd9ddfbbf0
Raman Prasad <raman_prasad@harvard.edu>
2019-06-25 13:55:47 -0400

    close for augment

--- dev_scripts/search_test3.py
+++ dev_scripts/search_test3.py
@@ -4,11 +4,6 @@ Reference:
 
 - https://datamart.d3m.vida-nyu.org
 """
-# from d3m import container
-#import datamart
-import datamart_nyu
-import datetime
-from pathlib import Path
 
 from io import BytesIO
 from collections import Counter
@@ -63,25 +58,29 @@ def run_augment_test():
 
     # Query result
     #
-    task_data = """{"id": "datamart.socrata.data-cityofnewyork-us.k2tc-bipg", "score": 8.788908, "metadata": {"name": "Medallion Taxi Initial Inspection Schedule", "description": "This is a schedule of Medallion Taxicab initial inspections at the Taxi and Limousine Commission\\u2019...", "size": 686334, "nb_rows": 13537, "columns": [{"name": "Medallion_Number", "structural_type": "http://schema.org/Text", "semantic_types": []}, {"name": "Schedule_Date", "structural_type": "http://schema.org/Text", "semantic_types": ["http://schema.org/DateTime"], "mean": 1566600510.95516, "stddev": 3029873.8282078225, "coverage": [{"range": {"gte": 1561334400.0, "lte": 1571788800.0}}]}, {"name": "Schedule_Time", "structural_type": "http://schema.org/Text", "semantic_types": ["http://schema.org/DateTime"], "mean": 1561359725.6562016, "stddev": 4632.38259222503, "coverage": [{"range": {"gte": 1561352400.0, "lte": 1561374000.0}}]}, {"name": "Fleet_Agent_Code", "structural_type": "http://schema.org/Integer", "semantic_types": [], "mean": 148.76346310113024, "stddev": 140.04502936441713, "coverage": [{"range": {"gte": 0.0, "lte": 999.0}}]}, {"name": "Last_Updated_Date", "structural_type": "http://schema.org/Text", "semantic_types": ["https://schema.org/Enumeration", "http://schema.org/DateTime"], "mean": 1561248000.0, "stddev": 0.0, "coverage": [{"range": {"gte": 1561248000.0, "lte": 1561248000.0}}]}, {"name": "Last_Updated_Time", "structural_type": "http://schema.org/Text", "semantic_types": ["https://schema.org/Enumeration", "http://schema.org/DateTime"], "mean": 1561399296.0, "stddev": 0.0, "coverage": [{"range": {"gte": 1561399200.766, "lte": 1561399200.766}}]}], "materialize": {"socrata_id": "k2tc-bipg", "socrata_domain": "data.cityofnewyork.us", "socrata_updated": "2019-06-23T22:10:10.000Z", "direct_url": "https://data.cityofnewyork.us/api/views/k2tc-bipg/rows.csv?accessType=DOWNLOAD", "identifier": "datamart.socrata", "date": "2019-06-24T02:01:08.225169Z"}, "date": "2019-06-24T03:19:48.375479Z"}, "augmentation": {"type": "none", "left_columns": [], "right_columns": []}}"""
+    task_data = {"id": "datamart.socrata.data-cityofnewyork-us.k2tc-bipg", "score": 8.788908, "metadata": {"name": "Medallion Taxi Initial Inspection Schedule", "description": "This is a schedule of Medallion Taxicab initial inspections at the Taxi and Limousine Commission\\u2019...", "size": 686334, "nb_rows": 13537, "columns": [{"name": "Medallion_Number", "structural_type": "http://schema.org/Text", "semantic_types": []}, {"name": "Schedule_Date", "structural_type": "http://schema.org/Text", "semantic_types": ["http://schema.org/DateTime"], "mean": 1566600510.95516, "stddev": 3029873.8282078225, "coverage": [{"range": {"gte": 1561334400.0, "lte": 1571788800.0}}]}, {"name": "Schedule_Time", "structural_type": "http://schema.org/Text", "semantic_types": ["http://schema.org/DateTime"], "mean": 1561359725.6562016, "stddev": 4632.38259222503, "coverage": [{"range": {"gte": 1561352400.0, "lte": 1561374000.0}}]}, {"name": "Fleet_Agent_Code", "structural_type": "http://schema.org/Integer", "semantic_types": [], "mean": 148.76346310113024, "stddev": 140.04502936441713, "coverage": [{"range": {"gte": 0.0, "lte": 999.0}}]}, {"name": "Last_Updated_Date", "structural_type": "http://schema.org/Text", "semantic_types": ["https://schema.org/Enumeration", "http://schema.org/DateTime"], "mean": 1561248000.0, "stddev": 0.0, "coverage": [{"range": {"gte": 1561248000.0, "lte": 1561248000.0}}]}, {"name": "Last_Updated_Time", "structural_type": "http://schema.org/Text", "semantic_types": ["https://schema.org/Enumeration", "http://schema.org/DateTime"], "mean": 1561399296.0, "stddev": 0.0, "coverage": [{"range": {"gte": 1561399200.766, "lte": 1561399200.766}}]}], "materialize": {"socrata_id": "k2tc-bipg", "socrata_domain": "data.cityofnewyork.us", "socrata_updated": "2019-06-23T22:10:10.000Z", "direct_url": "https://data.cityofnewyork.us/api/views/k2tc-bipg/rows.csv?accessType=DOWNLOAD", "identifier": "datamart.socrata", "date": "2019-06-24T02:01:08.225169Z"}, "date": "2019-06-24T03:19:48.375479Z"}, "augmentation": {"type": "none", "left_columns": [], "right_columns": []}}
+
+    task_data['augmentation'] = {\
+                    'type': 'join',
+                    'left_columns': [(0,)],
+                    'right_columns': [(0,)]
+                    #'left_columns': [('0', 0)],
+                    #'right_columns': [('0', 0)]
+                    #'left_columns': ['Medallion_Number'],
+                    #'right_columns': ['Medallion_Number']
+                    #'left_columns': [('Medallion_Number', 0)],
+                    #'right_columns': [('Medallion_Number', 0)]
+                    }
 
     # syntax check
     #
-    assert json.loads(task_data), 'task_data not valid json'
+    #assert json.loads(task_data), 'task_data not valid json'
 
     # Base file to augment.  Has two columns: Medallion_Number, Earnings
     #       2 columns x 20 rows
     #
     input_fpath = join(INPUT_DIR, 'medallion-test-file.csv')
     assert isfile(input_fpath), f'File not found: {input_fpath}'
-
-    # Set file for request
-    #
-    files = {'data': open(input_fpath, 'rb')}
-
-    # Set headers
-    #
-    headers = {"Content-Type": "multipart/form-data"}
 
     # Augment url
     #
@@ -92,14 +91,17 @@ def run_augment_test():
     print('augment_url', augment_url)
     print('-' * 40)
 
+    data_params = dict(data=open(input_fpath, 'rb'),
+                       task=json.dumps(task_data),
+                       )
+
     # Make request
     #
     try:
         response = requests.post(augment_url,
-                                 headers=headers,
-                                 files=files,
-                                 data=task_data,
+                                 files=data_params,
                                  verify=False,
+                                 allow_redirects=True,
                                  stream=True)
 
     except requests.exceptions.Timeout as err_obj:
