5311fd4e0f6c8de0d3c4cb38b93b7ee0991d042d
Shoeboxam <shoeboxam@gmail.com>
2019-06-27 14:48:24 -0400

    resolve rook crashing on restart

--- rook/rooksolver.R
+++ rook/rooksolver.R
@@ -13,9 +13,7 @@ timeout <- 20 # maximum time to spend on a univariate fit
 observationLimit <- 10
 
 # parallelize grid search
-# allowParallel=TRUE
 allowParallel=FALSE
-
 if (allowParallel) {
   library(parallel)
   library(doParallel)
