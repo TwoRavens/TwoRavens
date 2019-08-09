20a379b73b1a1a3fb14b5928f6890f2df81ca053
Shoeboxam <shoeboxam@gmail.com>
2019-08-09 00:17:48 -0400

    initial automl class structure/implementation
    This is not at the testing stage yet. Suggestions welcome

--- automl/runner.py
+++ automl/runner.py
@@ -1,15 +1,14 @@
-from automl.solver_auto_sklearn import solve_auto_sklearn
-from automl.solver_tpot import solve_tpot
-
 import sklearn.metrics
 
-import json
 import os
 import sys
+import uuid
 
 import flask
-from multiprocessing import Pool
-import uuid
+from multiprocessing import Pool, TimeoutError
+from multiprocessing.dummy import Pool as ThreadPool
+
+from automl.base import Solve, Search, Model
 
 NUM_PROCESSES = 4
 
@@ -31,20 +30,21 @@ def debug(*values):
     sys.stdout.flush()
 
 
-def score(metric, actual, fitted):
-    return {
-        'accuracy': sklearn.metrics.accuracy_score,
-        'recall': sklearn.metrics.recall_score,
-        'precision': sklearn.metrics.precision_score
-    }[metric](actual, fitted)
+def abortable_worker(func, *args, **kwargs):
+    timeout = kwargs.get('timeout', None)
+    p = ThreadPool(1)
+    res = p.apply_async(func, args=args)
+    try:
+        out = res.get(timeout)  # Wait timeout seconds for func to complete.
+        return out
+    except TimeoutError:
+        print("Aborting due to timeout")
+        p.terminate()
+        raise
 
 
-def task_handler(task):
-    return task['app'](task['problem'])
-
-
-@flask_app.route('/<solver_app>', methods=['POST'])
-def app_general(solver_app):
+@flask_app.route('/solve', methods=['POST'])
+def app_solve():
 
     data = flask.request.json
 
@@ -59,17 +59,84 @@ def app_general(solver_app):
 
     data['timeout'] = timeout
 
-    handler = pool.apply_async(task_handler, [{
-        'app': solver_app,
-        'data': data
-    }])
+    solver = Solve(
+        system=data['system'],
+        specification=data['specification'],
+        system_params=data['system_params'])
 
-    try:
-        resp = flask.Response(handler.get(timeout=timeout))
-        resp.headers['Content-Type'] = 'application/json'
-        return resp
-    except TimeoutError:
-        return json.dumps({KEY_SUCCESS: False, KEY_DATA: 'Timeout exceeded'})
+    # kick off solver, without awaiting, with a max timeout
+    pool.apply_async(lambda: abortable_worker(solver.run, timeout=timeout))
+
+    return {"search_id": solver.search.search_id}
+
+
+@flask_app.route('/search', methods=['POST'])
+def app_search():
+
+    data = flask.request.json
+
+    # sanity check timeout
+    if isinstance(data.get('timeout', None), (int, float)):
+        timeout = min(max(data.get('timeout'), 0), TIMEOUT_MAX)
+    else:
+        timeout = TIMEOUT_DEFAULT
+
+    data['timeout'] = timeout
+
+    search = Search.load(data['system'], data['specification'], data.get('system_params'))
+
+    # kick off solver, without awaiting, with a max timeout
+    pool.apply_async(lambda: abortable_worker(search.run, timeout=timeout))
+
+    return {"search_id": search.search_id}
+
+
+@flask_app.route('/describe', methods=['POST'])
+def app_describe():
+
+    data = flask.request.json
+
+    model = Model.load(data['model_id'])
+    return {
+        KEY_SUCCESS: True,
+        KEY_DATA: {
+            "search_id": model.search_id,
+            "model_id": model.model_id,
+            "description": model.describe()
+        }
+    }
+
+
+@flask_app.route('/produce', methods=['POST'])
+def app_produce():
+
+    data = flask.request.json
+
+    model = Model.load(data['model_id'])
+    return {
+        KEY_SUCCESS: True,
+        KEY_DATA: {
+            "search_id": model.search_id,
+            "model_id": model.model_id,
+            "produce": model.produce(data['specification'])
+        }
+    }
+
+
+@flask_app.route('/score', methods=['POST'])
+def app_score():
+
+    data = flask.request.json
+
+    model = Model.load(data['model_id'])
+    return {
+        KEY_SUCCESS: True,
+        KEY_DATA: {
+            "search_id": model.search_id,
+            "model_id": model.model_id,
+            "produce": model.score(data['specification'])
+        }
+    }
 
 
 if __name__ == '__main__':
@@ -84,35 +151,77 @@ if __name__ == '__main__':
 
 # ~~~~~ USAGE ~~~~~~
 
-print(task_handler({
-    'app': 'tpot',
-    'specification': {
-        # parameters specific for fitting the solver
-        'fit': {
-            'generations': 5
+# similar to D3M, but simplified
+specification = {
+    'search': {
+        "input": {
+            "resource_uri": 'file:///ravens_volume/test_data/185_baseball/TRAIN/dataset_TRAIN/tables/learningData.csv'
         },
+        'problem': {
+            "name": "problem 0",
+            "targets": ["Doubles"],
+            "predictors": ["At_bats", "Triples"],
+            "taskSubtype": "NONE",
+            "taskType": "CLASSIFICATION"
+        },
+        "performanceMetric": {
+            "metric": "F1_MACRO"
+        },
+        "configuration": {
+            "folds": 0,
+            "method": "K_FOLD",
+            "randomSeed": 0,
+            "shuffle": False,
+            "stratified": True,
+            "trainTestRatio": 0
+        },
+        "timeBoundSearch": 0,
+        "timeBoundRun": 0,
+        "rankSolutionsLimit": 0
+    },
 
-        # describe the search
-        'search': {
-            'id': uuid.uuid4(),
-            'dataset_path': 'file:///ravens_volume/test_data/185_baseball/TRAIN/dataset_TRAIN/tables/learningData.csv',
-            'problem': {
-                "target": "Doubles",
-                "predictors": ["At_bats", "Triples"],
-                "task": "regression"
+
+    'produce': [{
+        'input': {
+            'name': 'data_test',
+            "resource_uri": 'file:///ravens_volume/test_data/185_baseball/TEST/dataset_TEST/tables/learningData.csv'
+        },
+        'output': {
+            'resource_uri': 'file:///ravens_volume/test_output_auto_sklearn/185_baseball/'
+        }
+    }],
+
+    'score': [{
+        "input": {
+            "name": "data_test",
+            "resource_uri": 'file:///ravens_volume/test_data/185_baseball/TEST/dataset_TEST/tables/learningData.csv'
+        },
+        "performanceMetrics": [
+            {
+                "metric": "F1_MACRO"
             }
-        },
+        ]
+    }]
+}
 
-        # how to score a model
-        'score': [{
-            'dataset_path': 'file:///ravens_volume/test_data/185_baseball/TRAIN/dataset_TRAIN/tables/learningData.csv',
-            'metrics': [{'metric': 'accuracy'}]
-        }],
+sklearn_temp_path = '/ravens_volume/solvers/auto_sklearn/temporary/' + str(uuid.uuid4())
+tmp_folder = os.path.join(*sklearn_temp_path.split('/'), 'temp')
+output_folder = os.path.join(*sklearn_temp_path.split('/'), 'output')
 
-        # produce on a list of datasets
-        'produce': [{
-            'dataset_path': 'file:///ravens_volume/test_data/185_baseball/TRAIN/dataset_TRAIN/tables/learningData.csv',
-            'results_path': 'file:///ravens_volume/test_output_auto_sklearn/185_baseball/',
-        }]
-    }
-}))
+system_params = {
+    'auto_sklearn': {
+        'delete_tmp_folder_after_terminate': False,
+        'tmp_folder': tmp_folder,
+        'output_folder': output_folder
+    },
+    'tpot': {'generations': 5},
+    'h2o': {}
+}
+
+for solver_backend in system_params:
+    solver = Solve(
+        solver_backend,
+        specification,
+        system_params[solver_backend])
+    solver.run()
+
