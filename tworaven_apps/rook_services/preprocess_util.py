a4d7d0334f1286e570d884742eb77f04538fdb74
Raman Prasad <raman_prasad@harvard.edu>
2019-10-16 14:22:14 -0400

    May use python preprocess library.  Need to carry through UI updates

--- tworaven_apps/rook_services/preprocess_util.py
+++ tworaven_apps/rook_services/preprocess_util.py
@@ -9,17 +9,22 @@ putil = PreprocessUtil(src_file)
 if putil.has_error():
     print('error found: ', putil.get_error_message())
 else:
+    # Preprocess data as a python dict
     #
-    # Preprocess data as python dict
     print('preprocess data (python dict)', putil.get_preprocess_data())
 
-    # Preprocess data as JSON string
+    # Preprocess data as a JSON string
     #
     print('preprocess data (json string)', putil.get_preprocess_data_as_json())
 
-    # Preprocess data as JSON string indented 4 spaces
+    # Preprocess data as a JSON string indented 4 spaces
     #
     print('preprocess data (json string)', putil.get_preprocess_data_as_json(4))
+
+---
+10/2019 - Updated to use:
+    - rook preprocess OR
+    - tworavens-preprocess via pypi: https://pypi.org/project/tworavens-preprocess/
 
 """
 import csv
@@ -28,6 +33,9 @@ import json
 from os.path import isfile
 from datetime import datetime as dt
 import requests
+
+# source: https://pypi.org/project/tworavens-preprocess/
+from raven_preprocess.preprocess_runner import PreprocessRunner
 
 from tworaven_apps.data_prep_utils.duplicate_column_remover import DuplicateColumnRemover
 from tworaven_apps.utils.basic_err_check import BasicErrCheck
@@ -40,15 +48,23 @@ from tworaven_apps.utils.basic_response import (ok_resp,
                                                 err_resp)
 
 
+
 class PreprocessUtil(BasicErrCheck):
     """Convenience class for rook preprocess"""
     def __init__(self, source_path, **kwargs):
         """Takes a path to a data file and runs preprocess
 
-        - datastub is a unique directory that rookpreprocess uses to write
+        - datastub - a unique directory that rookpreprocess uses to write
+        - use_python_preprocess - boolean, default False.
+                - use python package for preprocessing.
         """
         self.source_path = source_path
+
+        # R preprocess specific value
         self.datastub = kwargs.get('datastub', None)
+
+        # Flag - should python preprocess be used
+        self.use_python_preprocess = kwargs.get('use_python_preprocess', False)
 
         # Option to read 1st line of file and fix duplicate columns names
         #
@@ -59,8 +75,11 @@ class PreprocessUtil(BasicErrCheck):
 
         self.column_names = None
 
-        self.set_rook_app_info()
-        self.run_preprocess()
+        if self.use_python_preprocess:
+            self.run_python_preprocess()
+        else:
+            self.set_rook_app_info()
+            self.run_preprocess()
 
 
     def get_preprocess_data_as_json(self, indent=None):
@@ -115,6 +134,44 @@ class PreprocessUtil(BasicErrCheck):
         return None
 
 
+    def run_python_preprocess(self):
+        """Run preprocess steps"""
+        if self.has_error():
+            return
+
+        # Make sure file exists
+        #
+        if not (self.source_path and isfile(self.source_path)):
+            self.add_error_message('File not found: %s' % self.source_path)
+            return
+
+        # Fix duplicate columns
+        #
+        if self.fix_duplicate_columns:
+            dcr = DuplicateColumnRemover(self.source_path)
+            self.column_names = dcr.updated_columns
+
+            self.column_names = [requests.utils.quote(column) for column in self.column_names]
+
+            if dcr.has_error():
+                user_msg = (f'Augment error during column checks: '
+                            f'{dcr.get_error_message()}')
+                self.add_error_message(user_msg)
+                return
+
+        # https://pypi.org/project/tworavens-preprocess/
+        #
+        run_info = PreprocessRunner.load_from_file(self.source_path)
+
+        if not run_info.success:
+            self.add_err_msg(run_info.err_msg)
+
+        runner = run_info.result_obj
+
+        # retrieve the data as a python OrderedDict
+        #
+        self.preprocess_data = runner.get_final_dict()
+
 
     def run_preprocess(self):
         """Run preprocess steps"""
@@ -165,6 +222,8 @@ class PreprocessUtil(BasicErrCheck):
             self.add_err_msg(err_msg)
             return
 
+        #  Convert rook text to an OrderedDict
+        #
         result_info = json_loads(rservice_req.text)
         if not result_info.success:
             user_msg = ('Failed to convert preprocess data '
