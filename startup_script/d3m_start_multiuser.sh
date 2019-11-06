9e979390c28f57c1a6f06cc92701c0f8d1d9130f
Raman Prasad <raman_prasad@harvard.edu>
2019-11-06 13:44:21 -0500

    Update d3m_start_multiuser.sh

--- startup_script/d3m_start_multiuser.sh
+++ startup_script/d3m_start_multiuser.sh
@@ -33,13 +33,13 @@ cd /var/webapps/TwoRavens
 printf "\n(10) clear any test data (shouldn't be any)"
 fab clear_test_data
 
-# (20) Make D3M config files
-printf "\n(20) Make D3M config files"
-fab make_d3m_configs_from_files_multiuser_test
-
-# (30) Copy test data to ravens_volume
+# (20) Copy test data to ravens_volume
 printf "\n(30) Copy data to ravens_volume"
 cp -r /var/webapps/TwoRavens/ravens_volume/. /ravens_volume/
+
+# (30) Make D3M config files
+printf "\n(20) Make D3M config files"
+fab make_d3m_configs_from_files_multiuser_test
 
 # (40) if it exists, attempt to load info from D3MINPUTDIR
 printf "\n(40) Copy data to ravens_volume"
