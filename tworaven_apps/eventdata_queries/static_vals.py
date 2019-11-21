161dac3228530698fbe5f17359d763436d216873
Raman Prasad <raman_prasad@harvard.edu>
2019-11-21 08:50:12 -0500

    temp test acled datasaet #746

--- tworaven_apps/eventdata_queries/static_vals.py
+++ tworaven_apps/eventdata_queries/static_vals.py
@@ -2,10 +2,7 @@
 
 FOLDER_COLLECTIONS = 'collections'
 
-ALL_COLLECTIONS_2019 = ['acled_africa.json',
-                        'acled_asia.json',
-                        'acled_middle_east.json',
-                        'cline_phoenix_fbis.json',
+ALL_COLLECTIONS_2019 = ['cline_phoenix_fbis.json',
                         'cline_phoenix_nyt.json',
                         'cline_phoenix_swb.json',
                         'cline_speed.json',
@@ -17,10 +14,14 @@ ALL_COLLECTIONS_2019 = ['acled_africa.json',
                         'phoenix_rt.json',
                         'terrier.json']
 
+ACLED_COLLECTIONS = ['acled_africa.json',
+                     'acled_asia.json',
+                     'acled_middle_east.json',]
+
 UT_DALLAS_COLLECTIONS = ['cline_phoenix_fbis.json',
                          'cline_phoenix_nyt.json',
                          'cline_phoenix_swb.json',
                          # 'cline_speed.json',
                          'icews.json',
                          'phoenix_rt.json',
-                         'terrier.json']
+                         'terrier.json'] + ACLED_COLLECTIONS
