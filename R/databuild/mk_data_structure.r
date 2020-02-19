5c09170d0682d9b8e6a482acd210bb687dd860e4
Raman Prasad <raman_prasad@harvard.edu>
2020-02-19 14:10:24 -0500

    update w/ develop version

--- R/databuild/mk_data_structure.r
+++ R/databuild/mk_data_structure.r
@@ -7,7 +7,7 @@ rm(list=ls())
 library(jsonlite)
 library(foreign)
 
-seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, citation="", description="", depvarname=NULL, taskType=NULL, taskSubType="", metric=NULL, seed=123){
+seed_skeleton <- function(data, name, samplesize=NULL, fraction=0.2, ID=NULL, citation="", description="", depvarname=NULL, taskType=NULL, taskSubType="", metric=NULL, seed=123){
 
   set.seed=seed
 
@@ -16,17 +16,22 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
   }
 
   myTargetIndex <- 0
-  myresID <- "0"
+  myresID <- "learningData"
 
 
-  # Subsample number of observations
+  # Sample number of observations
 
-  if(!is.null(subsample)){
-    data<-na.omit(data)
-    cat("subsampling dataset")
+  if(!is.null(samplesize)){
+    data[is.na(data)] <- 0  # Hack for present
+    #data<-na.omit(data)
+    cat("sampling dataset")
     ## NOTE: Should check there are enough cases to subsample
     ## NOTE: Should check subsample argument value is valid
-    myindex<-sample(1:nrow(data), size=subsample, replace=FALSE)
+    if (samplesize>nrow(data))
+      myreplace = TRUE
+    else
+      myreplace = FALSE
+    myindex<-sample(1:nrow(data), size=samplesize, replace=myreplace)
     data<-data[myindex,]
   }
 
@@ -37,8 +42,8 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
   data <- cbind(d3mIndex, data)
 
 
-  if(!(fraction>0) & !(fraction<1)){
-    print("Argument `fraction` should be 0 < fraction < 1 and represent the fraction of the data to put reserve for the test data.")
+  if(!(fraction>=0) & !(fraction<1)){
+    print("Argument `fraction` should be 0 <= fraction < 1 and represent the fraction of the data to put reserve for the test data.")
   }
 
 
@@ -60,13 +65,19 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
   ## Divide datasets and write out data
 
   n <- nrow(data)
-  testindex <- sample(x=1:n, size=ceiling(n*fraction), replace = FALSE,)
-  testdata <- data[testindex,]
-  traindata <- data[-testindex,]
+  if(fraction>0){
+    testindex <- sample(x=1:n, size=ceiling(n*fraction), replace = FALSE,)
+    testdata <- data[testindex,]
+    traindata <- data[-testindex,]
+    write.csv(data, paste(name, "/", name, "_dataset/tables/learningData.csv", sep=""), row.names=FALSE)
+    write.csv(testdata, paste(name, "/", "TEST/dataset_TEST/tables/learningData.csv", sep=""), row.names=FALSE)
+    write.csv(traindata, paste(name, "/", "TRAIN/dataset_TRAIN/tables/learningData.csv", sep=""), row.names=FALSE)
+  } else {
+    write.csv(data, paste(name, "/", name, "_dataset/tables/learningData.csv", sep=""), row.names=FALSE)
+    write.csv(data, paste(name, "/", "TRAIN/dataset_TRAIN/tables/learningData.csv", sep=""), row.names=FALSE)    
+  }
 
-  write.csv(data, paste(name, "/", name, "_dataset/tables/learningData.csv", sep=""), row.names=FALSE)
-  write.csv(testdata, paste(name, "/", "TEST/dataset_TEST/tables/learningData.csv", sep=""), row.names=FALSE)
-  write.csv(traindata, paste(name, "/", "TRAIN/dataset_TRAIN/tables/learningData.csv", sep=""), row.names=FALSE)
+
 
   ## Write various versions of datasetDoc.json
 
@@ -112,7 +123,7 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
 
   datasetID <- paste(ID,"_dataset",sep="")
 
-  datasetdoclist <- list(about=list(datasetID=name, datasetName=ID, description=description, citation=citation, datasetSchemaVersion= "3.0"), dataResources=list(dataResourcesList))
+  datasetdoclist <- list(about=list(datasetID=name, datasetName=ID, description=description, citation=citation, datasetSchemaVersion= "4.0.0"), dataResources=list(dataResourcesList))
   datasetDoc <- toJSON(datasetdoclist, auto_unbox=TRUE, pretty=TRUE)
 
   fileConn<-file(paste(name, "/", name, "_dataset/datasetDoc.json", sep=""))
@@ -130,7 +141,13 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
 
   ## Write various versions of datasetDoc.json
 
-  problemdoclist <- list(about=list(problemID=paste(name,"_problem",sep=""), problemName=ID, problemDescription=description, taskType=taskType, taskSubType=taskSubType, problemVersion="1.0", problemSchemaVersion="3.0"),
+  if(is.null(taskSubType)){
+    mytaskKeywords <- list(taskType)
+  } else {
+    mytaskKeywords <- list(taskType, taskSubType)
+  }
+
+  problemdoclist <- list(about=list(problemID=paste(name,"_problem",sep=""), problemName=ID, problemDescription=description, taskKeywords=mytaskKeywords, problemVersion="4.0.0", problemSchemaVersion="4.0.0"),
     inputs=list(data=list(list(datasetID=datasetID, targets=list(list(targetIndex= myTargetIndex, resID=myresID, colIndex=depvarColIndex, colName=depvarname)) )),
     dataSplits=list(method="holdOut", testSize=fraction), performanceMetrics=list(list(metric=metric))),
     expectedOutputs=list(predictionsFile="predictions.csv"))
@@ -154,19 +171,23 @@ seed_skeleton <- function(data, name, subsample=NULL, fraction=0.2, ID=NULL, cit
 
 
 
-ethdata <- read.csv("PHEM_Weekly_24.csv")
-#seed_skeleton(data=ethdata, name="TR10_Ethiopia_Health", subsample=1000, depvarname="TMalaria_OutP_Cases", ID="Ethiopia_Health", description="Emergency Health data at the weekly reporting level, by woreda", taskType="regression", taskSubType="", metric="rootMeanSquaredError")
-seed_skeleton(data=ethdata, name="TR13_Ethiopia_Health", subsample=1000, depvarname="TMalariaOutPCases", ID="Ethiopia_Health", description="Emergency Health data at the weekly reporting level, by woreda", taskType="regression", taskSubType="", metric="rootMeanSquaredError")
+#ethdata <- read.csv("PHEM_Weekly_24.csv")
+#seed_skeleton(data=ethdata, name="TR10_Ethiopia_Health", samplesize=1000, depvarname="TMalaria_OutP_Cases", ID="Ethiopia_Health", description="Emergency Health data at the weekly reporting level, by woreda", taskType="regression", taskSubType="", metric="rootMeanSquaredError")
+#seed_skeleton(data=ethdata, name="TR13_Ethiopia_Health", samplesize=1000, depvarname="TMalariaOutPCases", ID="Ethiopia_Health", description="Emergency Health data at the weekly reporting level, by woreda", taskType="regression", taskSubType="", metric="rootMeanSquaredError")
 
-fldata <- read.dta("repdata.dta")
-seed_skeleton(data=fldata, name="TR1_Greed_Versus_Grievance", depvarname="onset", ID="Greed_Versus_Grievance", description="Replication data for Fearon and Laitin greed versus grievance analysis", taskType="classification", taskSubType="binary", metric="f1Macro")
+#fldata <- read.dta("repdata.dta")
+#seed_skeleton(data=fldata, name="TR1_Greed_Versus_Grievance", depvarname="onset", ID="Greed_Versus_Grievance", description="Replication data for Fearon and Laitin greed versus grievance analysis", taskType="classification", taskSubType="binary", metric="f1Macro")
 
-chdata <- read.dta("sxprepdata.dta")
-seed_skeleton(data=chdata, name="TR2_Resource_Curse", depvarname="chwars", ID="Resource_Curse", description="Replication data for Collier and Hoeffler resource curse analysis", taskType="classification", taskSubType="multiClass", metric="f1Macro")
 
-pitfdata1<-read.delim("pitf_tab1_mod1.tsv")
-pitfdata2<-read.delim("pitf_tab3_modFL.tsv")
-pitfdata3<-read.delim("pitf_tab3_modPITF.tsv")
-seed_skeleton(data=pitfdata1, name="TR3a_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, primary model.", taskType="classification", taskSubType="binary", metric="f1Macro")
-seed_skeleton(data=pitfdata2, name="TR3b_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, with Fearon and Laitin comparison.", taskType="classification", taskSubType="binary", metric="f1Macro")
-seed_skeleton(data=pitfdata3, name="TR3c_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, primary model on Fearon Laitin observations", taskType="classification", taskSubType="binary", metric="f1Macro")
+#fldata <- read.dta("repdata.dta")
+#fldata$onset[fldata$onset==4] <- 1
+#seed_skeleton(data=fldata, samplesize=100000, name="TR24_Large_Scale_Greed", depvarname="onset", ID="Greed_Versus_Grievance", description="Replication data for Fearon and Laitin greed versus grievance analysis", taskType="classification", taskSubType="binary", metric="f1Macro")
+
+
+
+#pitfdata1<-read.delim("pitf_tab1_mod1.tsv")
+#pitfdata2<-read.delim("pitf_tab3_modFL.tsv")
+#pitfdata3<-read.delim("pitf_tab3_modPITF.tsv")
+#seed_skeleton(data=pitfdata1, name="TR3a_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, primary model.", taskType="classification", taskSubType="binary", metric="f1Macro")
+#seed_skeleton(data=pitfdata2, name="TR3b_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, with Fearon and Laitin comparison.", taskType="classification", taskSubType="binary", metric="f1Macro")
+#seed_skeleton(data=pitfdata3, name="TR3c_PITF", depvarname="sftpcons", ID="Forecasting_Political_Instability", description="Replication data for Goldstone et al. A Global Model for Forecasting Political Instability, primary model on Fearon Laitin observations", taskType="classification", taskSubType="binary", metric="f1Macro")
