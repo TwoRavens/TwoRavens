813b1354e49217e5c7b52b552ec201de7717c9ae
Raman Prasad <raman_prasad@harvard.edu>
2020-02-27 02:35:14 -0500

    Update static_vals.py

--- tworaven_apps/eventdata_queries/static_vals.py
+++ tworaven_apps/eventdata_queries/static_vals.py
@@ -22,13 +22,13 @@ ACLED_COLLECTIONS = ['acled_africa.json',
                      'acled_asia.json',
                      'acled_middle_east.json',]
 
-UT_DALLAS_COLLECTIONS = [#'cline_phoenix_fbis.json',
-                         #'cline_phoenix_nyt.json',
+UT_DALLAS_COLLECTIONS = ['cline_phoenix_fbis.json',
+                         'cline_phoenix_nyt.json',
                          'cline_phoenix_swb.json',
                          'cline_speed.json',
-                         'icews.json',
-                         'phoenix_rt.json',
-                         'terrier.json'] # + ACLED_COLLECTIONS
+                         'icews.json'] + ACLED_COLLECTIONS
+                         #'phoenix_rt.json',
+                         #'terrier.json'
 
 #import json
 #print('UT_DALLAS_COLLECTIONS', json.dumps(UT_DALLAS_COLLECTIONS))
