26765c7e564c6cbcf2d6eaf2fc1d218c0db2aa2c
Raman Prasad <raman_prasad@harvard.edu>
2020-05-07 14:27:39 -0400

    Solver integration (#857)
    * 1. Modified the util_model.py to collaborate changes in 2ravens_solver
    
    * 1. Fix an import bug that occurs after restart the server.
    
    * include monthly sunspots in dataset list, update tworaven_solver #854
    
    * Update local_image_build_2020.md
    
    Co-authored-by: yulin <linyu0077@gmail.com>

--- tworaven_apps/solver_interfaces/util_model.py
+++ tworaven_apps/solver_interfaces/util_model.py
@@ -697,17 +697,8 @@ class ModelTwoRavens(Model):
         dataframe = Dataset(score_specification['input']).get_dataframe()
 
         if self.task == "FORECASTING":
-            # dataframe_train = Dataset(score_specification['train']).get_dataframe()
-            # horizon = configuration.get('forecastingHorizon', {}).get('value', 1)
-            # if len(dataframe) < horizon:
-            #     raise ValueError(f'No predictions with a horizon of {horizon} are within range of the test data.')
-
-            predicted = self.model.predict(dataframe)
-
-            # predicted = self.forecast(
-            #     dataframe=dataframe_train,
-            #     dataframe_rolling=dataframe,
-            #     horizon=horizon)[:len(dataframe) - horizon + 1]
+            # ONLY consider default crossSection now
+            predicted = self.model.forecast(dataframe)
 
         elif self.task in ['CLASSIFICATION', 'REGRESSION']:
             # TODO: respect configuration on holdout vs cross-validation, do refitting, etc.
@@ -757,9 +748,10 @@ class ModelTwoRavens(Model):
             self.fit(dataframe=dataframe_train, data_specification=produce_specification['train'])
 
         if predict_type == 'RAW':
-            predicted = self.model.predict(dataframe)
-            # if len(predicted.columns.values) > 1:
-            #     predicted = np.argmax(predicted, axis=-1)
+            if "FORECASTING" == self.task:
+                predicted = self.model.forecast(dataframe)
+            else:
+                predicted = self.model.predict(dataframe)
         else:
             predicted = self.model.predict_proba(dataframe)
             # TODO: standardize probability column names
