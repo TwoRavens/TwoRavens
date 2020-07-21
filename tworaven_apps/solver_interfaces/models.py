d104b6c72e7a43074bd096f3772ecdec1349c975
yulin <linyu0077@gmail.com>
2020-07-21 17:31:25 -0500

    1. Fix score issue in classification task 2. Code clear

--- tworaven_apps/solver_interfaces/models.py
+++ tworaven_apps/solver_interfaces/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 import math
-import sklearn.metrics
+import sklearn.metrics as smetrics
 
 SAVED_MODELS_PATH = '/ravens_volume/solvers/models/'
 EXPORTED_MODELS_PATH = '/ravens_volume/solvers/exported/'
@@ -22,44 +22,108 @@ RECEIVE_PRODUCE_MSG = 'receive_produce_msg'
 RECEIVE_ERROR_MSG = 'receive_error_msg'
 
 
-def get_metric(specification):
+def get_metric(specification, aux_info=None):
+    def get_pos_label(classes_list):
+        # If 1 or True exist in the classes list, set it as pos_label
+        for item in classes_list:
+            if item == '1' or item.lower == 'true':
+                return item
+        return None
+
     if specification['metric'] == "ACCURACY":
-        return sklearn.metrics.accuracy_score
+        return smetrics.accuracy_score
     if specification['metric'] == "PRECISION":
-        return lambda actual, predicted: sklearn.metrics.precision_score(
-            actual, predicted, positive_label=specification.get('positiveLabel', 1))
+        cls_list = aux_info.get('clf_classes', None)
+        pos_label = get_pos_label(cls_list)
+        assert cls_list is not None, "Classes List should be available for classification task"
+
+        if len(cls_list) <= 2:
+            # Binary cls
+            return lambda actual, predicted: smetrics.precision_score(
+                actual, predicted,
+                labels=cls_list,
+                pos_label=pos_label if pos_label else cls_list[1],
+                average='binary')
+        else:
+            # Multi-class
+            return lambda actual, predicted: smetrics.precision_score(
+                actual, predicted,
+                labels=cls_list,
+                average='macro')
+
     if specification['metric'] == "RECALL":
-        return lambda actual, predicted: sklearn.metrics.recall_score(
-            actual, predicted, positive_label=specification.get('positiveLabel', 1))
-    if specification['metric'] == "F1":
-        return lambda actual, predicted: sklearn.metrics.f1_score(
-            actual, predicted, positive_label=specification.get('positiveLabel', 1))
-    if specification['metric'] == "F1_MICRO":
-        return lambda actual, predicted: sklearn.metrics.f1_score(actual, predicted, average="micro")
-    if specification['metric'] == "F1_MACRO":
-        return lambda actual, predicted: sklearn.metrics.f1_score(actual, predicted, average="macro")
-    if specification['metric'] == "ROC_AUC":
-        return sklearn.metrics.roc_auc_score
-    if specification['metric'] == "ROC_AUC_MICRO":
-        return lambda actual, predicted: sklearn.metrics.roc_auc_score(actual, predicted, average="micro")
-    if specification['metric'] == "ROC_AUC_MACRO":
-        return lambda actual, predicted: sklearn.metrics.roc_auc_score(actual, predicted, average="macro")
+        cls_list = aux_info.get('clf_classes', None)
+        pos_label = get_pos_label(cls_list)
+        assert cls_list is not None, "Classes List should be available for classification task"
+
+        if len(cls_list) <= 2:
+            # Binary cls
+            return lambda actual, predicted: smetrics.recall_score(
+                actual, predicted,
+                labels=cls_list,
+                pos_label=pos_label if pos_label else cls_list[1],
+                average='binary')
+        else:
+            # Multi-class
+            return lambda actual, predicted: smetrics.recall_score(
+                actual, predicted,
+                labels=cls_list,
+                average='macro')
+
+    if specification['metric'] in ["F1", "F1_MICRO", "F1_MACRO"]:
+        cls_list = aux_info.get('clf_classes', None)
+        pos_label = get_pos_label(cls_list)
+        assert cls_list is not None, "Classes List should be available for classification task"
+
+        if len(cls_list) <= 2:
+            # Binary cls
+            return lambda actual, predicted: smetrics.f1_score(
+                actual, predicted,
+                labels=cls_list,
+                pos_label=pos_label if pos_label else cls_list[1],
+                average='binary')
+        else:
+            # Multi-class
+            return lambda actual, predicted: smetrics.f1_score(
+                actual, predicted,
+                labels=cls_list,
+                average='micro' if specification["metric"] == "F1_MICRO" else 'macro')
+
+    if specification['metric'] in ["ROC_AUC", "ROC_AUC_MICRO", "ROC_AUC_MACRO"]:
+        cls_list = aux_info.get('clf_classes', None)
+        pos_label = get_pos_label(cls_list)
+        assert cls_list is not None, "Classes List should be available for classification task"
+
+        if len(cls_list) <= 2:
+            # Binary cls
+            return lambda actual, predicted: smetrics.roc_auc_score(
+                actual, predicted,
+                labels=cls_list,
+                average='micro' if specification['metric'] == "ROC_AUC_MICRO" else 'macro')
+        else:
+            # Multi-class
+            return lambda actual, predicted: smetrics.roc_auc_score(
+                actual, predicted,
+                labels=cls_list,
+                average='micro' if specification['metric'] == "ROC_AUC_MICRO" else 'macro',
+                multi_class='ovr')
+
     if specification['metric'] == 'ROOT_MEAN_SQUARED_ERROR':
-        return lambda actual, predicted: math.sqrt(sklearn.metrics.mean_squared_error(actual, predicted))
+        return lambda actual, predicted: math.sqrt(smetrics.mean_squared_error(actual, predicted))
     if specification['metric'] == "MEAN_SQUARED_ERROR":
-        return sklearn.metrics.mean_squared_error
+        return smetrics.mean_squared_error
     if specification['metric'] == "MEAN_ABSOLUTE_ERROR":
-        return sklearn.metrics.mean_absolute_error
+        return smetrics.mean_absolute_error
     if specification['metric'] == "R_SQUARED":
-        return sklearn.metrics.r2_score
+        return smetrics.r2_score
     if specification['metric'] == "JACCARD_SIMILARITY_SCORE":
-        return sklearn.metrics.jaccard_similarity_score
+        return smetrics.jaccard_similarity_score
     if specification['metric'] == "PRECISION_AT_TOP_K":
         raise NotImplementedError
     if specification['metric'] == "OBJECT_DETECTION_AVERAGE_PRECISION":
         raise NotImplementedError
     if specification['metric'] == "HAMMING_LOSS":
-        return sklearn.metrics.hamming_loss
+        return smetrics.hamming_loss
     if specification['metric'] == "RANK":
         raise NotImplementedError
 
